39499d77c13843bd56de1fe5f483ffa60e0fd457
Anil Anand <anilanandchess@gmail.com>
2025-01-06 21:00:20 +0530

    Modify `AddService` to handle custom delimiters in the input string

--- app/services/add_service.rb
+++ app/services/add_service.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class AddService
   def initialize(numbers_string)
     @numbers_string = numbers_string
@@ -13,13 +11,28 @@ class AddService
 
     def add(numbers)
       sum = 0
+      delimiter = ""
 
       return sum if numbers.empty?
 
+      delimiter = numbers.slice!(/\/\/(.+)\n/).strip[2..] if numbers.start_with?("//")
+
       current_number = 0
+      current_delimiter = ""
 
       numbers.each_char do |character|
-        current_number = current_number * 10 + character.to_i and next if character.match?(/\d/)
+        if character.match?(/\d/) && (current_delimiter.empty? || current_delimiter == delimiter)
+          current_number = current_number * 10 + character.to_i
+          current_delimiter = ""
+
+          next
+        end
+
+        if delimiter.present?
+          current_delimiter += character
+
+          next if current_delimiter != delimiter
+        end
 
         sum += current_number
         current_number = 0
