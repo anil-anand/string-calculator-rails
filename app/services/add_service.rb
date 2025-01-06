58ca49f74026c46313d07c8d69aed86fe6560583
Anil Anand <anilanandchess@gmail.com>
2025-01-06 20:14:04 +0530

    Modify `AddService` to handle the case of empty string inputs

--- app/services/add_service.rb
+++ app/services/add_service.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 class AddService
+  def initialize(numbers_string)
+    @numbers_string = numbers_string
+  end
+
+  def process
+    add(@numbers_string)
+  end
+
+  private
+
+    def add(numbers)
+      0 if numbers.empty?
+    end
 end
