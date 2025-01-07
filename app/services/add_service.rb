5919f430a7eb523fe5ef3116530f3cf867185354
Anil Anand <anilanandchess@gmail.com>
2025-01-07 21:42:35 +0530

    Refactor the whole logic of `AddService` to make it more readable and effecient

--- app/services/add_service.rb
+++ app/services/add_service.rb
@@ -1,66 +1,40 @@
 class AddService
-  def initialize(numbers_string)
-    @numbers = numbers_string
-    @sum = 0
+  def initialize(numbers)
+    @numbers = numbers
   end
 
   def process
     return 0 if @numbers.empty?
 
-    add
+    delimiter = detect_delimiter
+    number_list = extract_numbers(delimiter)
+
+    number_list.sum
   end
 
   private
 
-    def add
-      delimiter
-      current_number = 0
-      current_delimiter = ""
+  def detect_delimiter
+    return /[,\n]/ unless @numbers.start_with?("//")
 
-      @numbers.each_char do |character|
-        current_delimiter, current_number = process_character(character, current_number, current_delimiter)
-      end
+    custom_delimiter = @numbers.lines.first[2..-1].strip
+    @numbers = @numbers.lines[1..].join
+    Regexp.new(Regexp.escape(custom_delimiter))
+  end
 
-      @sum += current_number
-    end
+  def extract_numbers(delimiter)
+    parts = @numbers.split(delimiter)
+    validate_delimiters!(parts, delimiter.source)
+    parts.map(&:to_i)
+  end
 
-    def process_character(character, current_number, current_delimiter)
-      return "", current_number * 10 + character.to_i if digit_and_not_delimiter?(character, current_delimiter)
+  def validate_delimiters!(parts, delimiter)
+    invalid_delimiters = parts
+      .select { |part| part.to_i.to_s != part && part.match?(/[^0-9]/) && part != delimiter }
+      .map { |part| part.gsub(/[0-9]/, "") }
 
-      raise ArgumentError, argument_error_message(current_delimiter) if invalid_delimiter?(character, current_delimiter)
+    return if invalid_delimiters.empty?
 
-      if delimiter.present?
-        current_delimiter += character
-
-        return current_delimiter, current_number if current_delimiter != delimiter
-      end
-
-      @sum += current_number
-      return current_delimiter, 0
-    end
-
-    def digit_and_not_delimiter?(character, current_delimiter)
-      character.match?(/\d/) &&
-        (
-          current_delimiter.empty? ||
-          current_delimiter == delimiter
-        )
-    end
-
-    def invalid_delimiter?(character, current_delimiter)
-      character.match?(/\d/) &&
-      current_delimiter.present? &&
-      current_delimiter != delimiter &&
-      !delimiter.start_with?(current_delimiter)
-    end
-
-    def argument_error_message(current_delimiter)
-      "Invalid delimiter found in the string: #{current_delimiter[..-2]}"
-    end
-
-    def delimiter
-      @_delimiter ||= @numbers.start_with?("//") ?
-        @numbers.slice!(/\/\/(.+)\n/).strip[2..] :
-        ""
-    end
+    raise ArgumentError, "Invalid delimiters detected: #{invalid_delimiters.uniq.join(', ')}"
+  end
 end
