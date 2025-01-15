fd935be75e77a9d845f718dfc9dcbae6a3cee283
Anil Anand <anilanandchess@gmail.com>
2025-01-15 11:26:23 +0530

    Add test for `CalculatorController` to assert that addtion works when passing an odd or even choice

--- test/controllers/calculator_controller_test.rb
+++ test/controllers/calculator_controller_test.rb
@@ -7,4 +7,11 @@ class CalculatorControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal({ "result" => 6 }, JSON.parse(@response.body))
   end
+
+  def test_add_action_works_when_specifying_odd_or_even_preference
+    post calculator_add_url, params: { numbers: "1,2,3", odd_or_even: "odd" }
+
+    assert_response :success
+    assert_equal({ "result" => 2 }, JSON.parse(@response.body))
+  end
 end
